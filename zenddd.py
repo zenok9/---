9c2a93604629ccca00234656d01aade8e7553cbb
zenok9 <77606102+zenok9@users.noreply.github.com>
2021-01-18 13:11:01 +0600

    Update zenddd.py

--- zenddd.py
+++ zenddd.py
@@ -1,1 +1,15 @@
+import discord 
+from discord.ext import commands
 
+Bot = commands.Bot(command_prefix = "!")
+Bot.remove.command("help")
+
+@Bot.event
+async def on_read():
+  print("Bot is online.")
+  
+ @Bot.command(pass_content = true)
+async def ping():
+  await Bot.say("pong")
+  
+ Bot.run("NTA2NDE3OTk5ODkzMjMzNjY0.W9bkgw.EvC65A0DXSbfQsp4XtMPxZpVWtk")
